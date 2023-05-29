53594a1d3836ffa18f2412ace7f385c8a5640e33
Irene Ortega Tabuenca <irene.ortega2000@gmail.com>
2023-05-29 16:46:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/artonthemove/HelperClass.java
@@ -0,0 +1,54 @@
+package com.example.artonthemove;
+
+public class HelperClass {
+
+    String name, email, telephone, institution, password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HelperClass(String name, String email, String telephone, String institution, String password) {
+        this.name = name;
+        this.email = email;
+        this.telephone = telephone;
+        this.institution = institution;
+        this.password = password;
+    }
+}
